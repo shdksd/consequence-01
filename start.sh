fb619e9bc14f103f7996264a79f782b4e5167078
shdksd <89565185+shdksd@users.noreply.github.com>
2022-11-01 06:19:49 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # configs
-AUUID=bb76be97-b2f0-48de-a7b9-991383bb2454
+AUUID=e566fda4-139b-4f7b-9966-4bf9a238c43e
 CADDYIndexPage=https://github.com/shdksd/consequence-01/master.zip
 CONFIGCADDY=https://raw.githubusercontent.com/shdksd/consequence-01/master/etc/Caddyfile
 CONFIGXRAY=https://raw.githubusercontent.com/shdksd/consequence-01/master/etc/xray.json
